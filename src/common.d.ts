8aaa2b946a5a20af7fa3abf34d56431f4e2bc0c0
Masaki Yoshiiwa <yopinoji@gmail.com>
2021-09-18 23:17:14 +0900

    change component's name

--- src/common.d.ts
+++ src/common.d.ts
@@ -2,8 +2,6 @@
 
 declare module "*.svg" {
   import { FunctionComponent } from "react";
-
   const ReactComponent: FunctionComponent<React.SVGProps<SVGSVGElement>>;
-
   export default ReactComponent;
 }
--- src/components/atoms/Typography/index.ts
+++ src/components/atoms/Typography/index.ts
@@ -1,3 +1,3 @@
 export { H1 } from "./H1";
 export { H2 } from "./H2";
-export { BaseText } from "./BaseText";
+export { Paragraph } from "./Paragraph";
